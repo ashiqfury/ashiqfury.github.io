7596999b2717b527ab43f426f7c22fba5c7bbcfe
Fury <ashiqasraf07@gmail.com>
2022-02-23 20:43:31 +0530

    Modified: tailwind~ advice-generator / Frontend Mentor

---
+++ frontend-mentor/advice-generator/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			boxShadow: {
+				main: '30px 40px 5px rgba(32, 37, 45, 0.1), 10px 20px 5px 30px rgba(44, 50, 59, 0.1)',
+				anim: '0 0 10px hsl(150, 100%, 66%), 0 0 20px hsl(150, 100%, 66%)',
+			},
+			colors: {
+				primary: '#52ffa8',
+				dark: {
+					1: '#1b2027',
+					2: '#323b48',
+				},
+			},
+			fontFamily: {
+				custom: ['Manrope', 'sans-serif'],
+			},
+			backgroundImage: {
+				divider: "url('/images/pattern-divider-desktop.svg')",
+			},
+		},
+	},
+	plugins: [],
+}
